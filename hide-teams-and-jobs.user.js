788bcd353e8c8bf375f352a57dabd82e35e17e1c
a-stone-arachnid <31635665+a-stone-arachnid@users.noreply.github.com>
2018-09-08 06:07:31 -0700

    Update hide-teams-and-jobs.user.js

--- hide-teams-and-jobs.user.js
+++ hide-teams-and-jobs.user.js
@@ -3,35 +3,34 @@
 // @description   Remove all Jobs-related and Teams-related content from SO, making the main focus of the site Q/A
 // @author        a stone arachnid
 // @namespace     https://github.com/a-stone-arachnid/
-// @version       1.0
+// @version       1.1
 // @include       http*://*stackoverflow.com/*
 // @exclude       http*://chat.*.com/*
 // ==/UserScript==
 
 function with_jquery(f) 
 {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.textContent = "if (window.jQuery) (" + f.toString() + ")(window.jQuery)" + "\n\n//# sourceURL=" + encodeURI(GM_info.script.namespace.replace(/\/?$/, "/")) + encodeURIComponent(GM_info.script.name); // make this easier to debug
-  document.body.appendChild(script);
+  var s = document.createElement("script");
+  s.type = "text/javascript";
+  s.textContent = "if (window.jQuery) (" + f.toString() + ")(window.jQuery)" + "\n\n//# sourceURL=" + encodeURI(GM_info.script.namespace.replace(/\/?$/, "/")) + encodeURIComponent(GM_info.script.name); // make this easier to debug
+  document.body.appendChild(s);
 };
 
 with_jquery(function(){
-  $(".nav-links .fs-fine").hide();
-  $(".nav-links--link").css("padding-left","8px");
-  $("#nav-jobs").hide();
+  $("<style/>").text(`
+.nav-links .fs-fine, #nav-jobs, .nav-links .p6,
+.unread-feature-notice,.story-tab, #profile-side a[href^='/users/storypreferences'],
+#profile-side a[href^='/users/jobsearch'], #profile-side a[href*='job-alerts'],
+.site-footer--nav li.-item a[href^='https://stackoverflow.com/jobs'],
+.site-footer--nav li.-item a[href^='https://stackoverflow.com/teams']
+{
+	display:none!important
+}
+.nav-links .nav-links--link{
+	padding-left:8px;
+}
+`).appendTo("head");
   $("#nav-questions svg").hide();
   $("#nav-questions span").text("Questions");
-  $(".nav-links .p6").hide();
-
-  $(".unread-feature-notice").hide();
-  $(".story-tab").hide();
-
-  $("#profile-side a[href^='/users/storypreferences']").hide();
-  $("#profile-side a[href^='/users/jobsearch']").hide();
-  $("#profile-side a[href*='job-alerts']").hide();
-
-  $(".site-footer--nav li.-item a[href^='https://stackoverflow.com/jobs']").hide();
-  $(".site-footer--nav li.-item a[href^='https://stackoverflow.com/teams']").hide();
 
 });

6b0ff573229e9d9e7e60fee6977af76dcb432723
a-stone-arachnid <31635665+a-stone-arachnid@users.noreply.github.com>
2019-06-20 20:07:55 -0700

    Updated for new SE changes

--- no-more-jobs.user.js
+++ no-more-jobs.user.js
@@ -3,26 +3,19 @@
 // @description   Remove all Jobs-related and Teams-related content from SO, making the main focus of the site Q/A
 // @author        a stone arachnid
 // @namespace     https://github.com/a-stone-arachnid/
-// @version       1.2.0
-// @include       http*://*stackoverflow.com/*
-// @exclude       http*://chat.*.com/*
+// @version       1.3.0
+// @match         https://*.stackoverflow.com/*
+// @exclude       https://chat.*.com/*
 // ==/UserScript==
 
-function with_jquery(f) 
-{
-  var s = document.createElement("script");
-  s.type = "text/javascript";
-  s.textContent = "if (window.jQuery) (" + f.toString() + ")(window.jQuery)" + "\n\n//# sourceURL=" + encodeURI(GM_info.script.namespace.replace(/\/?$/, "/")) + encodeURIComponent(GM_info.script.name); // make this easier to debug
-  document.body.appendChild(s);
-};
-
-with_jquery(function(){
+StackExchange.ready(function(){
   $("<style/>").text(`
 .nav-links .fs-fine, #nav-jobs, .nav-links .p6,
 .unread-feature-notice,.story-tab, #profile-side a[href^='/users/storypreferences'],
 #profile-side a[href^='/users/jobsearch'], #profile-side a[href*='job-alerts'],
 .site-footer--nav li.-item a[href^='https://stackoverflow.com/jobs'], #hireme,
-.site-footer--nav li.-item a[href^='https://stackoverflow.com/teams']
+.site-footer--nav li.-item a[href^='https://stackoverflow.com/teams'],
+.-marketing-link, #left-sidebar ol > li:last-child+
 {
 	display:none!important
 }
